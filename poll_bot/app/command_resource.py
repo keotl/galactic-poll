b9ed1b1da618947080b100f6ad5ec87ee0021f9f
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-12-10 16:02:34 -0500

    url unquote question + answers

--- poll_bot/app/command_resource.py
+++ poll_bot/app/command_resource.py
@@ -1,3 +1,5 @@
+import urllib
+
 from jivago.lang.stream import Stream
 from jivago.wsgi.annotations import Resource
 from jivago.wsgi.methods import POST
@@ -9,13 +11,14 @@ class CommandResource(object):
     @POST
     def post(self, body: dict) -> dict:
         print(body)
-        args = body['text'].split("+")
+        args = urllib.parse.unquote(body['text']).split(" ")
         question = args[0]
         responses = args[1::]
         button_attachments = Stream(responses).map(
             lambda x: {"callback_id": "vote_callback", "name": "poll", "text": x, "type": "button",
                        "value": x}).toList()
         response = {"response_type": "in_channel",
-                    "attachments": [{"fallback": "poll button", "text": question, "callback_id": "vote_callback", "actions": button_attachments}]}
+                    "attachments": [{"fallback": "poll button", "text": question, "callback_id": "vote_callback",
+                                     "actions": button_attachments}]}
         print(response)
         return response
