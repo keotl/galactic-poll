59a9547bb58d699e4c486b5603375140c2c32b80
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-12-11 08:09:08 -0500

    multi vs single response poll

--- poll_bot/app/command_resource.py
+++ poll_bot/app/command_resource.py
@@ -2,7 +2,7 @@ import re
 import urllib
 
 from jivago.lang.stream import Stream
-from jivago.wsgi.annotations import Resource
+from jivago.wsgi.annotations import Resource, Path
 from jivago.wsgi.methods import POST
 
 
@@ -10,7 +10,16 @@ from jivago.wsgi.methods import POST
 class CommandResource(object):
 
     @POST
-    def post(self, body: dict) -> dict:
+    @Path("/multi-vote")
+    def multi_vote(self, body: dict) -> dict:
+        return self.post(body, callback_id="vote_callback_multi")
+
+    @POST
+    @Path("/single-vote")
+    def single_vote(self, body: dict) -> dict:
+        return self.post(body, callback_id="vote_callback")
+
+    def post(self, body: dict, callback_id="vote_callback") -> dict:
         unquoted_query = urllib.parse.unquote(body['text']).replace("+", " ")
         tokens = Stream(re.findall('"([^"]*)"', unquoted_query)).filter(lambda x: x != '+').toList()
 
@@ -18,7 +27,7 @@ class CommandResource(object):
         question = tokens[0]
         responses = tokens[1::]
         button_actions = Stream(responses).map(
-            lambda x: {"callback_id": "vote_callback", "name": "poll", "text": x, "type": "button",
+            lambda x: {"callback_id": callback_id, "name": "poll", "text": x, "type": "button",
                        "value": x}).toList()
 
         text = question
@@ -30,18 +39,18 @@ class CommandResource(object):
             splitted_attachments = [button_actions[i:i + 5] for i in
                                     range(0, len(button_actions), 5)]
             response = {"response_type": "in_channel",
-                        "attachments": [{"fallback": "poll button", "text": text, "callback_id": "vote_callback",
+                        "attachments": [{"fallback": "poll button", "text": text, "callback_id": callback_id,
                                          "actions": splitted_attachments[0]},
                                         *Stream(splitted_attachments[1::]).map(lambda x:
                                                                                {"fallback": "poll button", "text": "",
-                                                                                "callback_id": "vote_callback",
+                                                                                "callback_id": callback_id,
                                                                                 "actions": x}
                                                                                )
                                         ]}
         else:
 
             response = {"response_type": "in_channel",
-                        "attachments": [{"fallback": "poll button", "text": text, "callback_id": "vote_callback",
+                        "attachments": [{"fallback": "poll button", "text": text, "callback_id": callback_id,
                                          "actions": button_actions}]}
 
         return response
--- poll_bot/app/vote_callback_resource.py
+++ poll_bot/app/vote_callback_resource.py
@@ -2,7 +2,7 @@ import json
 import urllib
 
 from jivago.lang.stream import Stream
-from jivago.wsgi.annotations import Resource
+from jivago.wsgi.annotations import Resource, Path
 from jivago.wsgi.methods import POST
 
 
@@ -12,7 +12,7 @@ class VoteCallbackResource(object):
     @POST
     def vote_callback(self, body: dict) -> dict:
         payload = json.loads(urllib.parse.unquote(body["payload"]))
-
+        multi = payload['callback_id'] == 'vote_callback_multi'
         original_message = payload['original_message']
 
         message_body = urllib.parse.unquote(original_message['text']).replace("+", " ").split("\n")
@@ -28,35 +28,46 @@ class VoteCallbackResource(object):
         votes = {}
         for line in message_body[1::]:
             header, respondents = line.split(" -&gt; ")
+            if "`" in header:
+                header, _, _ = header.split("`")
+                header = header.strip(" ")
             if header not in votes:
                 votes[header] = []
             Stream(respondents.split(",")).forEach(lambda name: votes[header].append(name))
 
-        match = Stream(votes.items()).firstMatch(lambda option, voters: user_id in voters)
-        if match:
-            votes[match[0]].remove(user_id)
+        if not multi:
+            match = Stream(votes.items()).firstMatch(lambda option, voters: user_id in voters)
+            if match:
+                votes[match[0]].remove(user_id)
 
         if f"- {user_action}" not in votes:
             votes[f"- {user_action}"] = []
-        votes[f"- {user_action}"].append(user_id)
+        if user_id not in votes[f"- {user_action}"]:
+            votes[f"- {user_action}"].append(user_id)
+        elif user_id in votes[f"- {user_action}"]:
+            votes[f"- {user_action}"].remove(user_id)
 
         for line in message_body[1::]:
             header, respondents = line.split(" -&gt; ")
+            if "`" in header:
+                header, _, _ = header.split("`")
+                header = header.strip(" ")
             if header not in votes:
                 votes[header] = []
             respondents = ",".join(Stream(votes[header]).filter(lambda x: x not in ('', ' ')).toList())
-            new_message.append(f"{header} -> {respondents}")
+            number_of_votes = Stream(votes[header]).filter(lambda x: x not in ('', ' ')).count()
+            new_message.append(f"{header} `{number_of_votes}` -> {respondents}")
 
         original_message["attachments"][0]['text'] = "\n".join(new_message)
         for x in original_message['attachments']:
             x['actions'] = Stream(x['actions']).map(
-            lambda action: {
-                "id": action['id'],
-                "name": action['name'],
-                "text": action['text'].replace('+', " "),
-                "type": action['type'],
-                'value': action['value'].replace('+', " "),
-                "style": action['style']
-            }).toList()
+                lambda action: {
+                    "id": action['id'],
+                    "name": action['name'],
+                    "text": action['text'].replace('+', " "),
+                    "type": action['type'],
+                    'value': action['value'].replace('+', " "),
+                    "style": action['style']
+                }).toList()
 
         return original_message
